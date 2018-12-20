623a1cb9945223fca30280c1f72db97ed4c52cbe
Sapphire <sapphire5641@gmail.com>
2018-12-20 09:08:01 +0700

    add base files

---
+++ saffio_wv/w2v.py
@@ -0,0 +1,26 @@
+import torch
+
+class W2V(torch.nn.Module):
+    def __init__(self, tok_num, code_num):
+        super(W2V, self).__init__()
+        
+        self.embd = torch.nn.Embedding(
+            num_embeddings=tok_num, embedding_dim=code_num
+        )
+        self.fc = torch.nn.Sequential(
+        torch.nn.Linear(code_num, code_num, bias=False)
+        )
+
+        self.lo = torch.nn.NLLLoss()
+        
+    def forward(self, x):
+        em = self.embd(x)
+        de = self.fc(em)
+        log_de = torch.log_softmax(de, 1)
+        return log_de
+    
+    def loss(self, log_act, expected):
+        return self.lo(log_act, expected)
+    
+    def encode(self, x):
+        return self.embd(x)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="saffio_wv",
+    version="0.1.2",
+    description="wv",
+    url="",
+    author="Ivoria Nairov",
+    author_email="",
+    license="MIT",
+    packages=["saffio_wv"],
+    install_requires=[
+    ],
+    zip_safe=False
+)
